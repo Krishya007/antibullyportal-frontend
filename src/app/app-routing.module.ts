21f1b5ac6d17fcb9122e48d2d5f6330539e114a6
Aditya Tripathi <adityatripathi.at04@gmail.com>
2022-04-27 16:27:16 +0530

    added animations and 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FourZeroFourComponent } from './pages/four-zero-four/four-zero-four.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
-// const routes: Routes = [
-//   {
-//     path: '',
-//     component: HomeComponent,
-//   },
-//   {
-//     path: '**',
-//     component:
-//   },
-// ];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'reports',
+    component: ReportsComponent,
+  },
+  {
+    path: '**',
+    component: FourZeroFourComponent,
+  },
+];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { FourZeroFourComponent } from './pages/four-zero-four/four-zero-four.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, ReportsComponent, FourZeroFourComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
