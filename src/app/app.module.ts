7109495bb63d8ad558cf51339c0bfd129b8201cb
Aditya Tripathi <adityatripathi.at04@gmail.com>
2022-04-27 15:33:39 +0530

    Added Landing
    Co-authored-by: Prayag Jain <Prayag2@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/models/reports.ts
@@ -0,0 +1,9 @@
+export interface Article {
+  _id: string;
+  informantName: string;
+  informantContact: string;
+  victimName: string;
+  incidentDesc: string;
+  stepsTaken: string;
+  expectations: string;
+}
